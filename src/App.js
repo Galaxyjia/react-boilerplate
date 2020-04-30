9176205d4129f0a415baf80378edf541ca64d4db
Galaxyjia <328956238@qq.com>
2020-04-30 17:21:54 +0800

    basic dev

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React,{Component} from 'react';
 import moment from 'moment';
 import 'moment/locale/zh-cn';
-import Icon from '../public/icon.jpg';
-import "./style.css";
+
 
 class App extends Component {
     constructor(props) {
@@ -12,9 +11,7 @@ class App extends Component {
     render() {
         return (
             <div className="main">
-                <img src={Icon} /> 
                 <p>Musicook</p>
-                <div className="hello">你好</div>
                 <div>{moment().endOf('day').fromNow()}</div>
             </div>
         );
